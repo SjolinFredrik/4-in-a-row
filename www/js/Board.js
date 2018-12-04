1338424bd141da7dd993d544f091f5ce97981f20
Aleksandra <a.toholjevic@gmail.com>
2018-12-04 10:54:15 +0100

    moved super() into constructor

--- www/js/Board.js
+++ www/js/Board.js
@@ -1,8 +1,8 @@
 class Board extends Component {
 
-  super();
+  
   constructor() {
-
+    super();
     let markers = [];
     for (let row = 0; row < 6; row++) {
       for (let col = 0; col < 7; col++) {
