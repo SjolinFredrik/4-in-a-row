96a1197fcbed58c47192f20a1973c255456e9f22
Aleksandra <a.toholjevic@gmail.com>
2018-12-04 10:28:34 +0100

    class board, gamePage

---
+++ www/js/GamePage.js
@@ -0,0 +1,13 @@
+class GamePage extends Component {
+
+  super();
+
+  constructor(board, marker){
+    this.addRoute('/play', 'Spela');
+    this.board = board;
+    this.marker = marker;
+  }
+
+
+
+}
