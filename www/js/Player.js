6a82f50f6abeefad0d00f3c64cceebc9d9680a09
Fredrik Sjölin <sjolin.fredrik@gmail.com>
2018-12-05 11:57:34 +0100

    Added player.js / player.html
    Added 2 human players and 2 bot players in player.js. I gonna work with my template player.html in the gamePage.html, since I don't know how to integrate a template to another.

--- www/js/Player.js
+++ www/js/Player.js
@@ -1,7 +1,17 @@
-class Player {
+class Player extends Component {
 
-  constructor(){
+  constructor() {
     super();
 
+    this.playerNameOne = playerNameOne;
+    this.playerNameTwo = playerNameTwo;
+
+    this.botOne = botOne;
+    this.botTwo = botTwo;
+    
+    // Click event to add a player from the input page.
+    this.addEvents({
+      'click .add-player-btn': 'addPlayer'
+    });
   }
 }
