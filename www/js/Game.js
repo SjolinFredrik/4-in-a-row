5ab3a72c8abc9e02552a7c1f948063560c16a8a8
Fredrik Sjölin <sjolin.fredrik@gmail.com>
2018-12-05 15:02:34 +0100

    Added code to game.js

---
+++ www/js/Game.js
@@ -0,0 +1,10 @@
+class Game extends Component {
+
+  // players catches the created array in gamepage
+  constructor(players) {
+      super();
+      this.players = players;
+      this.board = new Board();
+      console.log(this);
+  }
+}
