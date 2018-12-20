ee60c1d65d6ea39d8ae1c282a1b8443436898d11
melli <melsie78@hotmail.com>
2018-12-20 14:20:03 +0100

    check HoriVertiDiagonal branch-fix
    had to fix code since previous commit, some deletes

--- www/js/Board.js
+++ www/js/Board.js
@@ -25,21 +25,23 @@ class Board extends Component {
       if (this.board[row][col].color === "") {
         this.board[row][col].color = this.game.currentPlayer.color;
         this.board[row][col].render();
+        this.checkForWin();
         this.game.changePlayer();
         break;
       }
     }
     return false;
   }
-  /*  //Horizontal 
-checkIfWin(){
-  this.color = 'red', 'yellow';
-  for (let color of colors);
-  for (let row= 0; row>= 6; row++);
-  for(let col=0; col>= 4; col++);
-  for (let i= 0; i>=3; i++);
-  this.board[row][col].value == i;
-  if ((this.board[row][col + 1].value == i);
+    
+// checkIfWin(){
+//   this.color = 'red', 'yellow';
+//   for (let color of colors);
+//   for (let row= 0; row>= 6; row++);
+//   for(let col=0; col>= 4; col++);
+//   for (let i= 0; i>=3; i++);
+//   this.board[row][col].value == i;
+//   if ((this.board[row][col + 1].value == i));
+// }
 
   checkForWin() {
     let winnerColor;
