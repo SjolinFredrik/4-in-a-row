f744bd3df46654c662f2dd543049e37398952e01
Zoe Ali <zoeyzali@gmail.com>
2018-12-31 16:41:46 +0100

    bot function

--- www/js/Bot.js
+++ www/js/Bot.js
@@ -1,10 +1,11 @@
 class Bot extends Component {
 
-  constructor (name, player1, player2) {
+  constructor (player1, player2) {
     super();
     this.name = 'computer' + name;
-    this.type = 'computer';
     this.bot = new Bot;
+    //
+    this.isHuman = false;
   }    
   }
   
