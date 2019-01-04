e4ad87c21660cc8e3f08e0eb9f7f07d9ca1a4234
melli <melsie78@hotmail.com>
2019-01-04 11:20:44 +0100

    added button clicks, json

--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -6,13 +6,15 @@ class PageContent extends Component {
     this.gamePage = new GamePage();
     this.highscorePage = new HighscorePage();
     this.missingPage = new MissingPage();
+    //App.highscorePage = this.highscorePage;
     this.loadHighscoreData();
 }
 
   async loadHighscoreData(){
     // optional, this is if we want to load data from a JSON file
-   JSON._classes(HighscorePage, HighscoreList, HighscoreItems);
+   JSON._classes(HighscorePage, HighscoreList, HighscoreItem);
    let response = await JSON._load('highscore.json');
+   console.log(response);
    if(response === null){ return; }
    // set the original id of highscorePage
    // to get things into the "rendering loop"
