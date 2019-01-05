f3c977b66f0eddbf692793ed9cdb7b8f2440e635
Zoe Ali <zoeyzali@gmail.com>
2019-01-05 23:03:32 +0100

    conflict resolve
    git-kris

--- www/js/Game.js
+++ www/js/Game.js
@@ -1,41 +1,38 @@
 class Game extends Component {
 
   // players catches the created array in gamepage
-  constructor(players, color) {
+  constructor(players, color, gamePage) {
     super();
     this.players = players;
-    this.changePlayer();
-    this.checkPlayerType();
-    this.board = new Board(this);
-
     //this.currentPlayer = players[0];
     //this.showPlayer = this.currentPlayer.name;
+    this.changePlayer();
+    
     //this.modalHide();
     //this.theWinnerIs();
+    this.board = new Board(this);
    // this.modal = new Modal('#modal', this);
- 
+   this.gamePage = gamePage;
+   this.addEvents({
+    'click .restart-button': 'startGame'
+  });
   }
 
   changePlayer() {
     this.currentPlayer = this.currentPlayer === this.players[0] ? this.players[1] : this.players[0];
-    this.showPlayer = this.currentPlayer.name
+    this.showPlayer = this.currentPlayer.name;
     this.render();
   }
-
   // method to check playertype for bot = isNotHuman
- checkPlayerType() {
+  checkPlayerType() {
     this.playerType = this.playerType === this.isNotHuman ? this.isNotHuman : this.currentPlayer;
-    this.bot = this.isNotHuman;
+    this.isNotHuman = this.players;
       console.log("hey bot");
       this.render();
-    }
-    }
 
-    // modalHide(){
+    }
+  }
+
+  // modalHide(){
   //   $('#modal').modal('hide');
-  //}
-
-  
-
-
-}
+  // }
--- www/js/GamePage.js
+++ www/js/GamePage.js
@@ -3,12 +3,14 @@ class GamePage extends Component {
   constructor() {
     super();
     this.addRoute('/play', 'Spela');
+   
     this.playersForm = new PlayersForm();
     this.addEvents({
       'click .start-game': 'startGame',
-      'click .restartButton': 'startGame'
+      'click .restart-button': 'startGame'
     });
     Store.gamePage = this;
+    
   }
   
   // create a method to start the game with 2 players, and put them
@@ -21,7 +23,7 @@ class GamePage extends Component {
     // Calls on the method for the validation that's created
     // in the PlayersForm.js, when the validation is a succsess the game starts.
     if (this.playersForm.validateInputNames()) {
-     this.game = new Game([player1, player2]);
+      this.game = new Game([player1, player2]);
     
       //adding navbar to Store variable to temporarilly store navbar 
       Store.navbar.showingCancelButton = true;
