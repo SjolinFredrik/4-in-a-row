f7ea037ad13c34d2818f4742226d44a285174e2d
melli <melsie78@hotmail.com>
2019-01-10 18:58:16 +0100

    high score styling

--- www/js/HighscorePage.js
+++ www/js/HighscorePage.js
@@ -28,30 +28,6 @@ class HighscorePage extends Component {
         this.render();
         JSON._save('highscore-data', {data: this});
     }
-// addItem() {
-// let winnerPlayer = this.game.currentPlayer.name;
-// let winnerScore = this.game.CurrentPlayer.moveNumber;
-// //let itemName = this.baseEl.find('.btn').val();
-// this.highscoreList.addItem(winnerPlayer, winnerScore);
-// this.update();
-// //this.baseEl.find('.btn').focus();
-// }
-
-// addHighscore(winnerName, winnerScore){
-//   this.highscoreList.addHighscore(winnerName, winnerScore);
-//   this.update();
-// }
-
-
-
-//   let highscoreList = [];
-//   //let input = "winnerform";
-//   let input = document.getElementById("input");
-//   let winner = (this.baseEl.find('#input').val());
-//   console.log('<li>' + winner + '</li>');
-
-//   $(highscoreList).push("#input").val;
-// }
 
 
 //TO DO:
